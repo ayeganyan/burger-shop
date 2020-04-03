dc73e2e9451f7ede717803ccf414d03807e930df
ayeganyan <ayeganyan92@gmail.com>
2020-04-03 17:36:59 +0400

    Added Ingridients component

---
+++ src/components/Burger/BurgerIngridients/BurgerIngridients.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+import classes from './BurgerIngredients.module.css'
+
+const ingredient = (props) => {
+    let ingredient = null
+    
+    switch(props.type) {
+        case 'bread-bottom':
+            ingredient = <div className={classes.BreadBottom}/>
+            break
+        case 'bread-top':
+            ingredient = (
+                <div className={classes.BreadTop}>
+                    <div className={classes.Seed1}/>
+                    <div className={classes.Seed2}/>
+                </div>
+            )
+            break
+        case 'meat':
+            ingredient = <div className={classes.Meat}/>
+            break
+        case 'cheese':
+            ingredient = <div className={classes.Cheese}/>
+            break
+        case 'salad':
+            ingredient = <div className={classes.Salad}/>
+            break
+        case 'bacon':
+            ingredient = <div className={classes.Bacon}/>
+            break
+        default:
+            console.log('Invalid ingredient: ' + props.tyoe)
+    }
+    return ingredient;
+}
+
+export default ingredient
