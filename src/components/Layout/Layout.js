acccf3e5d05d06df4d0771f8ab6c0e3162de8389
ayeganyan <ayeganyan92@gmail.com>
2020-04-06 00:12:59 +0400

    Added logo

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
 import Aux from '../../hocs/Aux'
+import Toolbar from '../Navigation/Toolbar/Toolbar'
 import classes from './Layout.module.css'
 
 const layout = (props) => (
     <Aux>
-        <div>Toolbar, sideDrawer, BackDrop</div>
+        <Toolbar/>
         <main className={classes.Content}>
             {props.children}
         </main>
