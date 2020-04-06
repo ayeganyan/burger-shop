c28d24f467d2e1e80d88c0051e1361529ea0ce26
ayeganyan <ayeganyan92@gmail.com>
2020-04-06 22:45:43 +0400

    Added sideDrawer for mobile devieces

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,16 +1,20 @@
 import React from 'react';
 
 import Logo from '../../Logo/Logo'
+import NavigationItems from '../NavigationItems/NavigationItems'
 import classes from './Toolbar.module.css'
 
 const Toolbar = () => {
     return (
         <header className={classes.Toolbar}>
             <div>MENU</div>
-            <Logo/>
-            <nav>
-                ...
-            </nav>
+
+            <div className={classes.DesktopOnly}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <NavigationItems />
+            </div>
         </header>
     );
 };
