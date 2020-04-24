d72b1b1d858a5934a0548f6abeb747bc3129e2ea
ayeganyan <ayeganyan92@gmail.com>
2020-04-24 16:26:51 +0400

    Added Checkout Summary page Added Checkout Summary page at the bottom to alter use routing

---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Burger from '../../Burger/Burger'
+import Button from '../../UI/Button/Button'
+
+import classes from './CheckoutSummary.module.css'
+
+const checkoutSummary = (props) => {
+    return (
+        <div className={classes.CheckoutSummary}>
+            <h1>Hope the burger is tasty!</h1>
+            <div style={{width: "100%", height: '300px', margin: 'auto'}}>
+                <Burger ingridients={props.ingredients}/>
+            </div>
+            <Button
+                btnType='Danger'>
+                    CANCEL
+            </Button>
+            <Button
+                btnType='Success'>
+                    CONTINUE
+            </Button>
+        </div>
+    );
+};
+
+export default checkoutSummary;
